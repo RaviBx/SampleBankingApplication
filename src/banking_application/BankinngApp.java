c01711ce875a8cdff8f3c5a18aae537b962b1418
Ravishan Balasooriya <ravimuay@gmail.com>
2021-08-25 22:22:19 +0530

    Added project

---
+++ src/banking_application/BankinngApp.java
@@ -0,0 +1,14 @@
+package banking_application;
+
+public class BankinngApp {
+
+	public static void main(String[] args) {
+		
+		
+		BankAccount obj1 = new BankAccount("XYZ","BA001");
+		
+		obj1.showMenu();
+
+	}
+
+}
